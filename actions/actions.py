d6bab2074646c560e8cdb1ef6b0d3fbf5c676118
Hrithik Neogi <56794641+Hrithik0699@users.noreply.github.com>
2021-12-13 20:16:33 +0530

    form implemented

--- actions/actions.py
+++ actions/actions.py
@@ -8,19 +8,43 @@
 # This is a simple example for a custom action which utters "Hello World!"
 from typing import Any, Text, Dict, List
 
-from typing import Any, Text, Dict, List
-
 from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet, EventType
 from rasa_sdk.executor import CollectingDispatcher
+import webbrowser
 
 
 
-class ActionSearch(Action):
 
+class InterviewForm(Action):
     def name(self) -> Text:
-        return "action_search"
+        return "interview_form"
 
-    def run(self, dispatcher: CollectingDispatcher,tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        required_slots = ["a1","a2","a3","a4","a5","a6","a7"]
+
+        for slot_name in required_slots:
+            if tracker.slots.get(slot_name) is None:
+                # The slot is not filled yet. Request the user to fill this slot next.
+                return [SlotSet("requested_slot", slot_name)]
+
+        # All slots are filled.
+        return [SlotSet("requested_slot", None)]
+
+class ActionSubmit(Action):
+    def name(self) -> Text:
+        return "action_submit"
+
+    def run(
+        self,
+        dispatcher,
+        tracker: Tracker,
+        domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+
+
         a1 = tracker.get_slot('a1')
         a2 = tracker.get_slot('a2')
         a3 = tracker.get_slot('a3')
@@ -28,14 +52,34 @@ class ActionSearch(Action):
         a5 = tracker.get_slot('a5')
         a6 = tracker.get_slot('a6')
         a7 = tracker.get_slot('a7')
-        a8 = tracker.get_slot('a8')
-        a9 = tracker.get_slot('a9')
-        a10 = tracker.get_slot('a10')
 
-        dispatcher.utter_message(text='Here are your search results'+str(a1)+","+str(a2)+"."+str(a3)+","+str(a4))
+        score=0
 
-        return []
+        if a1=='DDL(Data Definition Language)':
+            score+=1
+        if a2=='Conceptual Level':
+            score+=1
+        if a3=='Database Administrator':
+            score+=1
+        if a4=='Drop':
+            score+=1
+        if a5=='Savepoint':
+            score+=1
+        if a6=='overlay network':
+            score+=1
+        if a7=='unicast network':
+            score+=1
 
+
+        dispatcher.utter_message(template="utter_details_answer",
+                                 q1=tracker.get_slot("a1"),
+                                 q2=tracker.get_slot("a2"),
+                                 q3=tracker.get_slot("a3"),
+                                 q4=tracker.get_slot("a4"),
+                                 q5=tracker.get_slot("a5"),
+                                 q6=tracker.get_slot("a6"),
+                                 q7=tracker.get_slot("a7"),
+                                 Score=score)
 
 #from rasa_sdk import Action, Tracker
 # from rasa_sdk.executor import CollectingDispatcher
